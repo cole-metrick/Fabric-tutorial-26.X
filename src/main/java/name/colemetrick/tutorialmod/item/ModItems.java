4327ae4faf8047f198dd14fef5bf21069f0f4d54
cole-metrick <colemetrick@gmail.com>
2026-07-23 23:59:50 -0400

    Made monster findable in creative menu and updated its name

--- src/main/java/name/colemetrick/tutorialmod/item/ModItems.java
+++ src/main/java/name/colemetrick/tutorialmod/item/ModItems.java
@@ -2,11 +2,13 @@ package name.colemetrick.tutorialmod.item;
 
 import com.mojang.datafixers.types.Func;
 import name.colemetrick.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 
 import java.util.function.Function;
@@ -15,6 +17,7 @@ public class ModItems {
 
     //Registers an item to the item registry under a name and item properties. Top one allows for property initialization
     // public static final Item MONSTER = registerItem("monster", properties -> new Item(properties._______));
+    //name must be lowercase
     public static final Item MONSTER = registerItem("monster", Item::new);
 
     private static Item registerItem(String name, Function<Item.Properties, Item> function) {
@@ -24,5 +27,8 @@ public class ModItems {
 
     public static void registerModItems(){
         TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        //Add item to the creative mode tab specified
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.FOOD_AND_DRINKS).register(output -> {output.accept(MONSTER);});
     }
 }
